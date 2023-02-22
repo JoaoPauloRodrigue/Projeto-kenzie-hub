cfc7ae26fef096c2aebfea4747911dbfc3c6cd1e
João Paulo Rodrigues <rodrigues.jpfa@gmail.com>
2023-02-22 02:54:35 -0300

    feat/addUserContext: addUserContext and autoLogin

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,14 @@ import { ToastContainer } from "react-toastify";
 import { RoutesPage } from "./router/routes";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import { UserProvider } from "./Providers/UserContext";
 
 export const App = () => {
-  const [userLogin, setUserLogin] = useState(null);
-  const navigate = useNavigate();
-
-  const userLogOut = () => {
-    localStorage.removeItem("@TOKEN");
-    localStorage.removeItem("@USERID");
-    setUserLogin(null);
-    navigate("/");
-  };
-
   return (
     <div className="App">
-      <RoutesPage
-        setUserLogin={setUserLogin}
-        userLogin={userLogin}
-        userLogOut={userLogOut}
-      />
+      <UserProvider>
+        <RoutesPage />
+      </UserProvider>
       <ToastContainer
         position="top-left"
         autoClose={3000}
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -3,18 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "../components/pages/landingPage";
 import { RegisterPage } from "../components/pages/registerPage";
 import { DashboardPage } from "../components/pages/dashboardPage";
+import { ProtectedRoutes } from "../components/pages/ProtectedRoutes";
 
-export const RoutesPage = ({ userLogin, setUserLogin, userLogOut }) => {
+export const RoutesPage = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage setUserLogin={setUserLogin} />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route
-        path="/dashboard"
-        element={
-          <DashboardPage userLogin={userLogin} userLogOut={userLogOut} />
-        }
-      />
+
+      <Route path="/dashboard" element={<ProtectedRoutes />}>
+        <Route index element={<DashboardPage />} />
+      </Route>
     </Routes>
   );
 };
